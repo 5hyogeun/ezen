e1e6d7339019a7b2855aebb69927f189f0a356cb
ohhyogeun <ohhyogeun@khu.ac.kr>
2019-08-10 16:52:14 +0900

    190810 16:52

--- textmining/__init__.py
+++ textmining/__init__.py
@@ -1,5 +1,6 @@
 from textmining.model import SamsungReport
 
 if __name__ == '__main__':
-    f = SamsungReport.read_file() # static 이면 바로 호출 가능
-    print(SamsungReport.extract_hangeul(f))
+    sam = SamsungReport() # static이 아니라서 그럼
+    sam.download()
+    # print(sam.extract_noun())
